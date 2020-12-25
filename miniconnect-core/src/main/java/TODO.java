79feda40551b713e87792ea27efd6b804470469f
Dávid Horváth <horvath@webarticum.hu>
2020-12-25 09:14:37 +0100

    Extend TODO

--- miniconnect-core/src/main/java/TODO.java
+++ miniconnect-core/src/main/java/TODO.java
@@ -31,6 +31,7 @@ public interface TODO {
      * Complete REPL
      * 
      * <ul>
+     *   <li>better handling of quit vs semicolon etc.</li>
      *   <li>run the query on the connection</li>
      *   <li>print result set as table</li>
      *   <li>print error messages</li>
