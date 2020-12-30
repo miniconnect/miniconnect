38dd44f729b9ab5c0300f82831d33429d46b5574
Dávid Horváth <horvath@webarticum.hu>
2020-12-30 10:07:38 +0100

    Add new ideas to MiniValue

--- miniconnect-api/src/main/java/hu/webarticum/miniconnect/api/MiniValue.java
+++ miniconnect-api/src/main/java/hu/webarticum/miniconnect/api/MiniValue.java
@@ -2,9 +2,12 @@ package hu.webarticum.miniconnect.api;
 
 import java.io.InputStream;
 
+// FIXME: rename?
 public interface MiniValue {
     
     public boolean isNull();
+    
+    // FIXME: public byte[] retrieve(int start, int length); ?
 
     public int contentLength();
     
