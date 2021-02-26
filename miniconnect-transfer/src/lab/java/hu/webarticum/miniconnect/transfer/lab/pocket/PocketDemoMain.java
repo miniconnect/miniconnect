403cad6d7f332c3c83379b4a67b28aa5774c87af
Dávid Horváth <horvath@webarticum.hu>
2021-02-26 23:07:23 +0100

    Add a simple custom demo pocket

--- miniconnect-transfer/src/lab/java/hu/webarticum/miniconnect/transfer/lab/pocket/PocketDemoMain.java
+++ miniconnect-transfer/src/lab/java/hu/webarticum/miniconnect/transfer/lab/pocket/PocketDemoMain.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 import hu.webarticum.miniconnect.transfer.Block;
 import hu.webarticum.miniconnect.transfer.channel.BlockSource;
@@ -15,6 +18,7 @@ import hu.webarticum.miniconnect.transfer.fetcher.BlockSourceFetcher;
 import hu.webarticum.miniconnect.transfer.fetcher.CollectingConsumer;
 import hu.webarticum.miniconnect.transfer.fetcher.CollectingNotifier;
 import hu.webarticum.miniconnect.transfer.fetcher.DecodingBlockConsumer;
+import hu.webarticum.miniconnect.transfer.fetcher.pocket.Pocket;
 import hu.webarticum.miniconnect.transfer.fetcher.pocket.SingleItemPocket;
 import hu.webarticum.miniconnect.transfer.util.ByteString;
 
@@ -31,17 +35,29 @@ public class PocketDemoMain {
                 item -> System.out.println(String.format("FALLBACK: %s", item)));
         
         try (BlockSourceFetcher fetcher = createFetcher(source, collectingConsumer)) {
-            SingleItemPocket<String> pocket = new SingleItemPocket<>(item -> item.equals("bbb"));
-            CollectingNotifier<String> notifier = collectingConsumer.listen(pocket);
+            SingleItemPocket<String> bbbPocket = new SingleItemPocket<>(item -> item.equals("bbb"));
+            CollectingNotifier<String> bbbNotifier = collectingConsumer.listen(bbbPocket);
+            
+            FirstNFilteredPocket firstTwoNumbersPocket =
+                    new FirstNFilteredPocket(2, item -> item.matches("\\d+"));
+            CollectingNotifier<String> firstTwoNumbersNotifier =
+                    collectingConsumer.listen(firstTwoNumbersPocket);
+            
             Thread thread = sendMessagesOnOtherThread(target);
             
-            synchronized (notifier) {
-                while (!pocket.isCompleted()) {
-                    notifier.wait();
+            synchronized (bbbNotifier) {
+                while (!bbbPocket.isCompleted()) {
+                    bbbNotifier.wait();
                 }
             }
-            
-            System.out.println(String.format("FOUND: %s", pocket.get()));
+            System.out.println(String.format("FOUND BBB: %s", bbbPocket.get()));
+
+            synchronized (firstTwoNumbersNotifier) {
+                while (!firstTwoNumbersPocket.isCompleted()) {
+                    firstTwoNumbersNotifier.wait();
+                }
+            }
+            System.out.println(String.format("FOUND NUMBERS: %s", firstTwoNumbersPocket.get()));
             
             thread.join();
         }
@@ -55,9 +71,13 @@ public class PocketDemoMain {
 
     private static void sendMessages(BlockTarget target) {
         try {
+            target.send(blockOf("111"));
             target.send(blockOf("aaa"));
+            target.send(blockOf("222"));
             target.send(blockOf("bbb"));
+            target.send(blockOf("333"));
             target.send(blockOf("ccc"));
+            target.send(blockOf("444"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -80,6 +100,52 @@ public class PocketDemoMain {
     public static Block blockOf(String string) {
         ByteString content = ByteString.wrap(string.getBytes(StandardCharsets.UTF_8));
         return new Block(content);
+    }
+    
+    
+    private static class FirstNFilteredPocket implements Pocket<String> {
+        
+        private final int n;
+        
+        private final Predicate<String> filter;
+        
+        private final List<String> items = new ArrayList<>();
+        
+        
+        private FirstNFilteredPocket(int n, Predicate<String> filter) {
+            this.n = n;
+            this.filter = filter;
+        }
+
+
+        @Override
+        public AcceptStatus accept(String item) {
+            int size;
+            synchronized (this) {
+                size = items.size();
+            }
+            
+            if (size >= n || !filter.test(item)) {
+                return Pocket.AcceptStatus.DECLINED;
+            }
+            
+            synchronized (this) {
+                items.add(item);
+            }
+            
+            return size == n - 1 ?
+                    Pocket.AcceptStatus.COMPLETED :
+                    Pocket.AcceptStatus.ACCEPTED;
+        }
+
+        public synchronized boolean isCompleted() {
+            return (items.size() == n);
+        }
+
+        public synchronized List<String> get() {
+            return new ArrayList<>(items);
+        }
+
     }
 
 }
