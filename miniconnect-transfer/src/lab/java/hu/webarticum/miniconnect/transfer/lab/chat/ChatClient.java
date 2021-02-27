8df373e49147c81fd0d7647e4d0e1ca942aba3ca
Dávid Horváth <horvath@webarticum.hu>
2021-02-27 20:32:31 +0100

    Implement a simple client-server chat application

---
+++ miniconnect-transfer/src/lab/java/hu/webarticum/miniconnect/transfer/lab/chat/ChatClient.java
@@ -0,0 +1,90 @@
+package hu.webarticum.miniconnect.transfer.lab.chat;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+
+import hu.webarticum.miniconnect.transfer.channel.BlockSource;
+import hu.webarticum.miniconnect.transfer.channel.BlockTarget;
+import hu.webarticum.miniconnect.transfer.fetcher.BlockSourceFetcher;
+import hu.webarticum.miniconnect.transfer.fetcher.DecodingBlockConsumer;
+
+public class ChatClient implements Closeable {
+
+    private static final String SELF_PROMPT = "you > ";
+    
+    private static final String FORMAT = "         %s: %s%n";
+    
+    
+    private final BufferedReader in;
+    
+    private final Appendable out;
+    
+    private final String senderName;
+    
+    private final BlockSourceFetcher fetcher;
+    
+    private final BlockTarget target;
+    
+    
+    private ChatClient(
+            BufferedReader in,
+            Appendable out,
+            String senderName,
+            BlockSource source,
+            BlockTarget target) {
+
+        this.in = in;
+        this.out = out;
+        this.senderName = senderName;
+        this.target = target;
+        
+        DecodingBlockConsumer<ChatMessage> consumer =
+                new DecodingBlockConsumer<>(ChatMessage::decode, this::accept);
+        this.fetcher = BlockSourceFetcher.start(source, consumer);
+    }
+    
+    public static ChatClient start(
+            BufferedReader in,
+            Appendable out,
+            String senderName,
+            BlockSource source,
+            BlockTarget target) {
+        return new ChatClient(in, out, senderName, source, target);
+    }
+    
+    
+    public void loop() throws IOException {
+        out.append(SELF_PROMPT);
+        while (true) { // NOSONAR
+            String message = in.readLine();
+            send(message);
+            out.append(String.format(FORMAT, senderName, message));
+            out.append(SELF_PROMPT);
+        }
+    }
+    
+    public void accept(ChatMessage chatMessage) {
+        try {
+            out.append('\n');
+            out.append(String.format(
+                    FORMAT,
+                    chatMessage.senderName(),
+                    chatMessage.message()));
+            out.append(SELF_PROMPT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void send(String message) throws IOException {
+        ChatMessage chatMessage = new ChatMessage(senderName, message);
+        target.send(chatMessage.encode());
+    }
+
+    @Override
+    public void close() throws IOException {
+        fetcher.close();
+    }
+
+}
