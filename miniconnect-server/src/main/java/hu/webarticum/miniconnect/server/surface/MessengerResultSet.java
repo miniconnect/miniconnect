9fa1fd1caa10310a50b7700d964b29e2a2c53369
Dávid Horváth <horvath@webarticum.hu>
2021-06-01 07:39:48 +0200

    Add skeleton of MessengerResultSet

---
+++ miniconnect-server/src/main/java/hu/webarticum/miniconnect/server/surface/MessengerResultSet.java
@@ -0,0 +1,36 @@
+package hu.webarticum.miniconnect.server.surface;
+
+import java.io.IOException;
+
+import hu.webarticum.miniconnect.api.MiniColumnHeader;
+import hu.webarticum.miniconnect.api.MiniResultSet;
+import hu.webarticum.miniconnect.api.MiniValue;
+import hu.webarticum.miniconnect.util.data.ImmutableList;
+
+// TODO: implement
+public class MessengerResultSet implements MiniResultSet {
+
+    @Override
+    public ImmutableList<MiniColumnHeader> columnHeaders() {
+        
+        // TODO
+        return null;
+        
+    }
+
+    @Override
+    public ImmutableList<MiniValue> fetch() throws IOException {
+        
+        // TODO
+        return null;
+        
+    }
+
+    @Override
+    public void close() throws IOException {
+        
+        // TODO
+        
+    }
+
+}
