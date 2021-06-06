ca15f5fa7a4c5a2d79190109215d51810cc04da0
Dávid Horváth <horvath@webarticum.hu>
2021-06-06 21:33:30 +0200

    Add MessengerLobAccessMain

---
+++ miniconnect-server/src/lab/java/hu/webarticum/miniconnect/server/lab/lob/MessengerLobAccessMain.java
@@ -0,0 +1,74 @@
+package hu.webarticum.miniconnect.server.lab.lob;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+import hu.webarticum.miniconnect.server.message.response.ResultSetValuePartResponse;
+import hu.webarticum.miniconnect.server.surface.MessengerLobAccess;
+import hu.webarticum.miniconnect.util.data.ByteString;
+
+public class MessengerLobAccessMain {
+
+    private static final int SUPPLIER_SLEEP_MILLIS = 1000;
+    
+    private static final int[] SUPPLIER_ORDER = new int[] {3, 2, 0, 6, 1, 5, 4, 8, 7};
+    
+    private static final int SUPPLIER_CHUNK_SIZE = 7;
+    
+    private static final int CONSUMER_CHUNK_SIZE = 10;
+    
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        long fullLength = ((long) SUPPLIER_CHUNK_SIZE) * SUPPLIER_ORDER.length;
+        try (MessengerLobAccess lob =
+            new MessengerLobAccess(fullLength, Files.createTempFile("LOB_", ".bin").toFile())) {
+            
+            Thread supplierThread = new Thread(() -> {
+                for (int n : SUPPLIER_ORDER) {
+                    try {
+                        Thread.sleep(SUPPLIER_SLEEP_MILLIS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                    long offset = ((long) n) * SUPPLIER_CHUNK_SIZE;
+                    char letter = (char) ('a' + n);
+                    StringBuilder contentBuilder = new StringBuilder();
+                    for (int i = 0; i < SUPPLIER_CHUNK_SIZE; i++) {
+                        contentBuilder.append(letter);
+                    }
+                    String contentString = contentBuilder.toString();
+                    System.out.println("Write: " + contentString);
+                    ByteString content = ByteString.of(contentString);
+                    ResultSetValuePartResponse partResponse =
+                            new ResultSetValuePartResponse(0L, 0, 0L, 0, offset, content);
+                    try {
+                        lob.accept(partResponse);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            
+            supplierThread.start();
+
+            try (InputStream in = lob.inputStream()) {
+                byte[] buffer = new byte[CONSUMER_CHUNK_SIZE];
+                while (true) {
+                    int r = in.read(buffer);
+                    if (r == (-1)) {
+                        break;
+                    }
+                    
+                    String part = new String(buffer, 0, r);
+                    System.out.println(" Fetched: " + part);
+                }
+            }
+            
+            supplierThread.join();
+        }
+    }
+    
+}
