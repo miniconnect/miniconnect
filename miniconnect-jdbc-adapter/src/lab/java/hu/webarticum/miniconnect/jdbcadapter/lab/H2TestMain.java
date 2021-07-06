199d9fb0ca06765d1add42802a746af82c509470
Dávid Horváth <horvath@webarticum.hu>
2021-07-06 18:40:06 +0200

    Add parameters to H2TestMain

--- miniconnect-jdbc-adapter/src/lab/java/hu/webarticum/miniconnect/jdbcadapter/lab/H2TestMain.java
+++ miniconnect-jdbc-adapter/src/lab/java/hu/webarticum/miniconnect/jdbcadapter/lab/H2TestMain.java
@@ -1,7 +1,9 @@
 package hu.webarticum.miniconnect.jdbcadapter.lab;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import hu.webarticum.miniconnect.api.MiniSession;
 import hu.webarticum.miniconnect.jdbcadapter.JdbcAdapterSession;
@@ -14,11 +16,23 @@ public class H2TestMain {
     
     private static final String JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
     
+    private static final String JDBC_USERNAME = "";
+    
+    private static final String JDBC_PASSWORD = "";
+    
+    private static final String SET_STATEMENT = "SET @%s = ?";
+    
 
     public static void main(String[] args) throws Exception {
-        try (Connection jdbcConnection = DriverManager.getConnection(JDBC_URL)) {
+        runRepl(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD, SET_STATEMENT);
+    }
+    
+    private static void runRepl(String url, String username, String password, String setStatement
+            ) throws SQLException, IOException {
+        
+        try (Connection jdbcConnection = DriverManager.getConnection(url, username, password)) {
             SimpleJdbcLargeDataPutter largeDataPutter =
-                    new SimpleJdbcLargeDataPutter("SET @%s = ?");
+                    new SimpleJdbcLargeDataPutter(setStatement);
             try (MiniSession session = new JdbcAdapterSession(jdbcConnection, largeDataPutter)) {
                 Repl repl = new SqlRepl(
                         session,
