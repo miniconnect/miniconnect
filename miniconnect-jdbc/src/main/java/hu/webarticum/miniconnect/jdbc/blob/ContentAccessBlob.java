d8c7321f2b907e6b83fc696402d0212c6550988a
Dávid Horváth <horvath@webarticum.hu>
2021-07-21 06:44:27 +0200

    Make ContentAccessBlob read-only, add tests and fixes

--- miniconnect-jdbc/src/main/java/hu/webarticum/miniconnect/jdbc/blob/ContentAccessBlob.java
+++ miniconnect-jdbc/src/main/java/hu/webarticum/miniconnect/jdbc/blob/ContentAccessBlob.java
@@ -1,6 +1,5 @@
 package hu.webarticum.miniconnect.jdbc.blob;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Blob;
@@ -8,42 +7,16 @@ import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
 
 import hu.webarticum.miniconnect.api.MiniContentAccess;
-import hu.webarticum.miniconnect.tool.contentaccess.ChargeableContentAccess;
-import hu.webarticum.miniconnect.tool.contentaccess.FileChargeableContentAccess;
-import hu.webarticum.miniconnect.tool.contentaccess.MemoryChargeableContentAccess;
-import hu.webarticum.miniconnect.util.data.ByteString;
 
 public class ContentAccessBlob implements Blob {
     
-    private static final int MAX_MEMORY_CONTENT_SIZE = 10 * 1024 * 1024;
-    
-    
     private final MiniContentAccess contentAccess;
-    
-    private volatile boolean completed;
     
     
     public ContentAccessBlob(MiniContentAccess contentAccess) {
-        this(contentAccess, true);
-    }
-
-    public ContentAccessBlob(long length) {
-        this(createChargeableContentAccess(length), false);
-    }
-
-    private ContentAccessBlob(MiniContentAccess contentAccess, boolean completed) {
         this.contentAccess = contentAccess;
-        this.completed = completed;
     }
     
-    private static ChargeableContentAccess createChargeableContentAccess(long length) {
-        if (length > MAX_MEMORY_CONTENT_SIZE) {
-            return new FileChargeableContentAccess(length);
-        } else {
-            return new MemoryChargeableContentAccess((int) length);
-        }
-    }
-
 
     @Override
     public long length() throws SQLException {
@@ -52,21 +25,9 @@ public class ContentAccessBlob implements Blob {
 
     @Override
     public byte[] getBytes(long pos, int length) throws SQLException {
-        if (!isAvailable(pos, length)) {
-            throw new SQLException("Content part is not available");
-        }
-        return contentAccess.get(pos, length).extract();
+        return contentAccess.get(pos - 1, length).extract();
     }
     
-    public boolean isAvailable(long pos, long length) {
-        if (completed) {
-            return true;
-        }
-        
-        ChargeableContentAccess chargeableContentAccess = (ChargeableContentAccess) contentAccess;
-        return chargeableContentAccess.isAvailable(pos, length);
-    }
-
     @Override
     public InputStream getBinaryStream() throws SQLException {
         return contentAccess.inputStream();
@@ -74,7 +35,7 @@ public class ContentAccessBlob implements Blob {
 
     @Override
     public InputStream getBinaryStream(long pos, long length) throws SQLException {
-        return contentAccess.inputStream(pos, length);
+        return contentAccess.inputStream(pos - 1, length);
     }
 
     @Override
@@ -89,38 +50,22 @@ public class ContentAccessBlob implements Blob {
 
     @Override
     public int setBytes(long pos, byte[] bytes) throws SQLException {
-        return setBytes(pos, bytes, 0, bytes.length);
+        throw createReadOnlyException();
     }
 
     @Override
     public int setBytes(long pos, byte[] bytes, int offset, int len) throws SQLException {
-        if (completed) {
-            throw new SQLException("This BLOB is already completed");
-        }
-        
-        long zeroBasedPosition = pos - 1;
-        ChargeableContentAccess chargeableContentAccess = (ChargeableContentAccess) contentAccess;
-        try {
-            chargeableContentAccess.accept(zeroBasedPosition, ByteString.of(bytes, offset, len));
-        } catch (Exception e) {
-            throw new SQLException(e);
-        }
-        
-        return len;
+        throw createReadOnlyException();
     }
 
     @Override
     public OutputStream setBinaryStream(long pos) throws SQLException {
-        if (pos != 1) {
-            throw new SQLException("Start position must be 1");
-        }
-        
-        return new MiniJdbcBlobOutputStream();
+        throw createReadOnlyException();
     }
 
     @Override
     public void truncate(long len) throws SQLException {
-        throw new SQLFeatureNotSupportedException();
+        throw createReadOnlyException();
     }
 
     @Override
@@ -128,32 +73,9 @@ public class ContentAccessBlob implements Blob {
         contentAccess.close();
     }
     
-    // FIXME: this is not thread safe
-    private class MiniJdbcBlobOutputStream extends OutputStream {
-
-        private long oneBasedPosition = 1;
-        
-        
-        @Override
-        public void write(int b) throws IOException {
-            write(new byte[] { (byte) b });
-        }
-
-        @Override
-        public void write(byte[] b) throws IOException {
-            write(b, 0, b.length);
-        }
-
-        @Override
-        public void write(byte[] b, int off, int len) throws IOException {
-            try {
-                setBytes(oneBasedPosition, b);
-            } catch (Exception e) {
-                throw new IOException(e);
-            }
-            oneBasedPosition += len;
-        }
-        
+    
+    private SQLException createReadOnlyException() {
+        return new SQLException("This BLOB is read-only");
     }
-
+    
 }
---
+++ miniconnect-jdbc/src/test/java/hu/webarticum/miniconnect/jdbc/blob/ContentAccessBlobTest.java
@@ -0,0 +1,44 @@
+package hu.webarticum.miniconnect.jdbc.blob;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.nio.charset.StandardCharsets;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import hu.webarticum.miniconnect.api.MiniContentAccess;
+import hu.webarticum.miniconnect.tool.result.StoredContentAccess;
+import hu.webarticum.miniconnect.util.data.ByteString;
+
+class ContentAccessBlobTest {
+
+    @Test
+    void testContents() throws Exception {
+        MiniContentAccess contentAccess = new StoredContentAccess(ByteString.of("abcdefghijk"));
+        Blob blob = new ContentAccessBlob(contentAccess);
+
+        assertThat(blob.length()).isEqualTo(11L);
+        assertThat(blob.getBytes(1L, 11)).containsExactly(ByteString.of("abcdefghijk").extract());
+        assertThat(blob.getBytes(2L, 3)).containsExactly(ByteString.of("bcd").extract());
+        assertThat(blob.getBinaryStream()).hasBinaryContent(ByteString.of("abcdefghijk").extract());
+        assertThat(blob.getBinaryStream(3L, 4L)).hasBinaryContent(
+                ByteString.of("cdef").extract());
+    }
+
+    @Test
+    void testReadOnly() throws Exception {
+        MiniContentAccess contentAccess = new StoredContentAccess(ByteString.of("abcdefghijk"));
+        Blob blob = new ContentAccessBlob(contentAccess);
+        
+        assertThatThrownBy(() -> blob.setBytes(1, ByteString.of("xyz").extract()))
+                .isInstanceOf(SQLException.class);
+        assertThatThrownBy(() -> blob.setBytes(1, ByteString.of("xyz").extract(), 1, 2))
+                .isInstanceOf(SQLException.class);
+        assertThatThrownBy(() -> blob.setBinaryStream(1)).isInstanceOf(SQLException.class);
+        assertThatThrownBy(() -> blob.truncate(3L)).isInstanceOf(SQLException.class);
+    }
+
+}
