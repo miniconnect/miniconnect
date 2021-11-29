c0d5a4da4f201c15371e0fb2747da65f145c1699
Dávid Horváth <horvath@webarticum.hu>
2021-11-29 03:27:09 +0100

    Add @FunctionalInterface to Messenger

--- miniconnect-messenger/src/main/java/hu/webarticum/miniconnect/messenger/Messenger.java
+++ miniconnect-messenger/src/main/java/hu/webarticum/miniconnect/messenger/Messenger.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 import hu.webarticum.miniconnect.messenger.message.request.Request;
 import hu.webarticum.miniconnect.messenger.message.response.Response;
 
+@FunctionalInterface
 public interface Messenger {
 
     public void accept(Request request, Consumer<Response> responseConsumer);
