9885e200a9fb6668b3097a669ee5641dd6ce6cc5
Dávid Horváth <horvath@webarticum.hu>
2021-11-29 02:42:30 +0100

    Remove obsolete TODO comment

--- miniconnect-tool/src/main/java/hu/webarticum/miniconnect/tool/repl/ReplRunner.java
+++ miniconnect-tool/src/main/java/hu/webarticum/miniconnect/tool/repl/ReplRunner.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 
-// TODO: handle incomplete lines
 public class ReplRunner implements Runnable {
 
     private final Repl repl;
