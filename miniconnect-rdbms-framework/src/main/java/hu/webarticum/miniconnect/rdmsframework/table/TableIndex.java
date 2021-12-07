6262434aae65d763b7ea00d0ca28676f7016da5c
Dávid Horváth <horvath@webarticum.hu>
2021-12-07 21:38:31 +0100

    Add column names to TableIndex

--- miniconnect-rdbms-framework/src/main/java/hu/webarticum/miniconnect/rdmsframework/table/TableIndex.java
+++ miniconnect-rdbms-framework/src/main/java/hu/webarticum/miniconnect/rdmsframework/table/TableIndex.java
@@ -6,6 +6,8 @@ import hu.webarticum.miniconnect.rdmsframework.database.NamedResource;
 import hu.webarticum.miniconnect.util.data.ImmutableList;
 
 public interface TableIndex extends NamedResource {
+    
+    public ImmutableList<String> columnNames();
 
     public boolean isUnique();
 
