6e250687678eb98a27313a1ddee51f7e5ec73042
Dávid Horváth <horvath@webarticum.hu>
2021-12-13 05:17:47 +0100

    Add partial implementation of ScanningTableIndex

--- miniconnect-rdbms-framework/src/main/java/hu/webarticum/miniconnect/rdmsframework/storage/impl/simpletable/ScanningTableIndex.java
+++ miniconnect-rdbms-framework/src/main/java/hu/webarticum/miniconnect/rdmsframework/storage/impl/simpletable/ScanningTableIndex.java
@@ -1,5 +1,10 @@
 package hu.webarticum.miniconnect.rdmsframework.storage.impl.simpletable;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import hu.webarticum.miniconnect.rdmsframework.storage.Table;
 import hu.webarticum.miniconnect.rdmsframework.storage.TableIndex;
 import hu.webarticum.miniconnect.rdmsframework.storage.TableSelection;
@@ -14,14 +19,46 @@ public class ScanningTableIndex implements TableIndex {
     
     private final ImmutableList<String> columnNames;
     
+    private final ImmutableList<Integer> columnIndexes;
+    
+    private final Comparator<Object> comparator; // FIXME???
+    
     
     public ScanningTableIndex(Table table, String name, ImmutableList<String> columnNames) {
+        this(table, name, columnNames, null);
+    }
+
+    public ScanningTableIndex(
+            Table table,
+            String name,
+            ImmutableList<String> columnNames,
+            Comparator<Object> comparator) {
         this.table = table;
         this.name = name;
         this.columnNames = columnNames;
+        this.columnIndexes = collectColumnIndexes(table, columnNames);
+        this.comparator = comparator;
     }
     
+    private static ImmutableList<Integer> collectColumnIndexes(
+            Table table,
+            ImmutableList<String> columnNames) {
+        ImmutableList<String> tableColumnNames = table.columns().names();
+        int width = columnNames.size();
+        Integer[] indexes = new Integer[width];
+        for (int i = 0; i < width; i++) {
+            String columnName = columnNames.get(i);
+            int columnIndex = tableColumnNames.indexOf(columnName);
+            if (columnIndex == -1) {
+                throw new IllegalArgumentException(
+                        String.format("Column not found: '%s'", columnName));
+            }
+            indexes[i] = columnIndex;
+        }
+        return ImmutableList.of(indexes);
+    }
     
+
     public Table table() {
         return table;
     }
@@ -48,10 +85,87 @@ public class ScanningTableIndex implements TableIndex {
             ImmutableList<?> to,
             boolean toInclusive,
             boolean sort) {
+        BigInteger tableSize = table.size();
+        List<SortHelper> foundRowEntries = new ArrayList<>();
+        for (
+                BigInteger i = BigInteger.ZERO;
+                i.compareTo(tableSize) < 0;
+                i = i.add(BigInteger.ONE)) {
+            ImmutableList<Object> row = table.row(i);
+            ImmutableList<Object> values = extractValues(row);
+            if (checkValues(values, from, fromInclusive, to, toInclusive)) {
+                foundRowEntries.add(new SortHelper(i, values));
+            }
+        }
+        
+        if (sort) {
+            // TODO
+            // Collections.sort(foundRowEntries, e -> ...);
+        }
+
+        // FIXME
+        Object orderKey = sort ? new Object() : table.rowOrderKey();
+        
+        // FIXME
+        return new SimpleSelection(
+                orderKey,
+                new ImmutableList<>(foundRowEntries).map(e -> e.index));
+        
+    }
+    
+    private ImmutableList<Object> extractValues(ImmutableList<Object> row) {
+        List<Object> resultBuilder = new ArrayList<>(columnIndexes.size());
+        for (int columnIndex : columnIndexes) {
+            Object value = row.get(columnIndex);
+            resultBuilder.add(value);
+        }
+        return new ImmutableList<>(resultBuilder);
+    }
+    
+    private boolean checkValues(
+            ImmutableList<Object> values,
+            ImmutableList<?> from,
+            boolean fromInclusive,
+            ImmutableList<?> to,
+            boolean toInclusive) {
+        if (from.equals(to)) { // FIXME: handle find(single), else use comparator
+            return isPrefixOf(from, values);
+        }
         
         // TODO
-        return new SimpleSelection(new Object(), ImmutableList.empty());
+        return false;
+    }
+    
+    private boolean isPrefixOf(ImmutableList<?> prefix, ImmutableList<?> values) {
+        int prefixWidth = prefix.size();
+        if (prefixWidth > values.size()) {
+            return false;
+        }
         
+        for (int i = 0; i < prefixWidth; i++) {
+            Object prefixValue = prefix.get(i);
+            Object value = values.get(i);
+            if (!value.equals(prefixValue)) { // FIXME: comparator?
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    private static class SortHelper {
+        
+        private final BigInteger index;
+        
+        private final ImmutableList<Object> values;
+
+        
+        private SortHelper(BigInteger index, ImmutableList<Object> values) {
+            this.index = index;
+            this.values = values;
+        }
+
     }
     
 }
--- miniconnect-rdbms-framework/src/test/java/hu/webarticum/miniconnect/rdmsframework/storage/impl/simpletable/ScanningTableIndexTest.java
+++ miniconnect-rdbms-framework/src/test/java/hu/webarticum/miniconnect/rdmsframework/storage/impl/simpletable/ScanningTableIndexTest.java
@@ -1,16 +1,68 @@
 package hu.webarticum.miniconnect.rdmsframework.storage.impl.simpletable;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import hu.webarticum.miniconnect.rdmsframework.storage.Table;
+import hu.webarticum.miniconnect.rdmsframework.storage.TableSelectionEntry;
+import hu.webarticum.miniconnect.rdmsframework.storage.impl.fakecolumn.FakeColumnDefinition;
+import hu.webarticum.miniconnect.util.data.ImmutableList;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigInteger;
 
 class ScanningTableIndexTest {
+    
+    private Table table;
+    
+
+    @BeforeEach
+    void init() {
+        table = SimpleTable.builder()
+                .addColumn("id", new FakeColumnDefinition())
+                .addColumn("firstname", new FakeColumnDefinition())
+                .addColumn("lastname", new FakeColumnDefinition())
+                .addColumn("country", new FakeColumnDefinition())
+                .addRow(ImmutableList.of(1, "Sándor", "Petőfi", "Hungary"))
+                .addRow(ImmutableList.of(2, "Adam", "Smith", "England"))
+                .addRow(ImmutableList.of(3, "Will", "Smith", "USA"))
+                .addRow(ImmutableList.of(4, "Karl", "Marx", "Germany"))
+                .addRow(ImmutableList.of(4, "Anton", "Bruckner", "Germany"))
+                .addRow(ImmutableList.of(5, "Anonymus", null, "Hungary"))
+                .build();
+    }
 
     @Test
-    void testXXX() {
-        
-        // TODO
-        
+    void testNonExistingColumn() {
+        assertThatThrownBy(() -> index("lorem")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testNotFound() {
+        assertThat(index("id").findValue(99)).isEmpty();
+    }
+    
+    @Test
+    void testFindSingle() {
+        assertThat(index("id").findValue(3)).map(TableSelectionEntry::tableIndex)
+                .containsExactly(bigs(2));
+    }
+    
+    // TODO
+    
+    
+    private ScanningTableIndex index(String... columnNames) {
+        return new ScanningTableIndex(table, "index", ImmutableList.of(columnNames));
+    }
+    
+    private BigInteger[] bigs(int... values) {
+        BigInteger[] result = new BigInteger[values.length];
+        for (int i = 0; i < values.length; i++) {
+            result[i] = BigInteger.valueOf(values[i]);
+        }
+        return result;
     }
     
 }
