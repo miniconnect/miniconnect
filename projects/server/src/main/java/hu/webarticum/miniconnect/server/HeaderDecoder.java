674ec4ef7cc4a300f87c40b9deaab08882705e01
Dávid Horváth <horvath@webarticum.hu>
2022-01-15 16:32:10 +0100

    Add explanatory doc comment to HeaderDecoder.

--- projects/server/src/main/java/hu/webarticum/miniconnect/server/HeaderDecoder.java
+++ projects/server/src/main/java/hu/webarticum/miniconnect/server/HeaderDecoder.java
@@ -1,7 +1,20 @@
 package hu.webarticum.miniconnect.server;
 
+import hu.webarticum.miniconnect.transfer.Packet;
 import hu.webarticum.miniconnect.util.data.ByteString;
 
+/**
+ * Packet header decoder.
+ * 
+ * <p>Deserializes a {@link Packet} from its standard byte representation.</p>
+ * 
+ * <p>It is separated from {@link MessageDecoder} because it is more shared.
+ * More application can be interested in decoding and reading headers
+ * than other parts of the message content.</p>
+ * 
+ * @see HeaderEncoder
+ * @see HeaderData
+ */
 public class HeaderDecoder {
 
     public HeaderData decode(ByteString headerBytes) {
