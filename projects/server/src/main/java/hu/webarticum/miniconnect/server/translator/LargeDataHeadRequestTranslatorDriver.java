1cd5d8301ba755381ad44569eb12e1162eab4fb3
Dávid Horváth <horvath@webarticum.hu>
2022-01-21 05:58:53 +0100

    Implement LargeDataHeadRequestTranlatorDriver

--- projects/server/src/main/java/hu/webarticum/miniconnect/server/translator/LargeDataHeadRequestTranslatorDriver.java
+++ projects/server/src/main/java/hu/webarticum/miniconnect/server/translator/LargeDataHeadRequestTranslatorDriver.java
@@ -1,29 +1,43 @@
 package hu.webarticum.miniconnect.server.translator;
 
+import java.nio.charset.StandardCharsets;
+
 import hu.webarticum.miniconnect.messenger.message.Message;
 import hu.webarticum.miniconnect.messenger.message.request.LargeDataHeadRequest;
 import hu.webarticum.miniconnect.server.HeaderData;
+import hu.webarticum.miniconnect.server.HeaderEncoder;
 import hu.webarticum.miniconnect.transfer.Packet;
 import hu.webarticum.miniconnect.util.data.ByteString;
 
 public class LargeDataHeadRequestTranslatorDriver implements TranslatorDriver {
 
     @Override
-    public Message decode(HeaderData header, ByteString payload) {
-        // TODO Auto-generated method stub
-        return null;
+    public Message decode(HeaderData headerData, ByteString payload) {
+        ByteString.Reader reader = payload.reader();
+        int variableNameBytesLength = reader.readInt();
+        byte[] variableNameBytes = reader.read(variableNameBytesLength);
+        String variableName = new String(variableNameBytes, StandardCharsets.UTF_8);
+        long length = reader.readLong();
+        return new LargeDataHeadRequest(
+                headerData.sessionId(),
+                headerData.exchangeId(),
+                variableName,
+                length);
     }
 
     @Override
     public Packet encode(Message message) {
         LargeDataHeadRequest largeDataHeadRequest = (LargeDataHeadRequest) message;
-        HeaderData header = HeaderData.ofMessage(message);
-        
-        // TODO variableName;
-        // TODO length;
-
-        // TODO Auto-generated method stub
-        return null;
+        HeaderData headerData = HeaderData.ofMessage(message);
+        ByteString header = new HeaderEncoder().encode(headerData);
+        byte[] variableNameBytes = largeDataHeadRequest.variableName()
+                .getBytes(StandardCharsets.UTF_8);
+        ByteString payload = ByteString.builder()
+                .appendInt(variableNameBytes.length)
+                .append(variableNameBytes)
+                .appendLong(largeDataHeadRequest.length())
+                .build();
+        return Packet.of(header, payload);
     }
 
 }
