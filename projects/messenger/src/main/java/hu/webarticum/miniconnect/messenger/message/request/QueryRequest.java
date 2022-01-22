0ccbdb7be1535f70bd74f323bdca14d250c57938
Dávid Horváth <horvath@webarticum.hu>
2022-01-22 19:03:53 +0100

    Add hashCode(), equals() and toString() to QueryRequest

--- projects/messenger/src/main/java/hu/webarticum/miniconnect/messenger/message/request/QueryRequest.java
+++ projects/messenger/src/main/java/hu/webarticum/miniconnect/messenger/message/request/QueryRequest.java
@@ -1,6 +1,9 @@
 package hu.webarticum.miniconnect.messenger.message.request;
 
+import java.util.Objects;
+
 import hu.webarticum.miniconnect.messenger.message.ExchangeMessage;
+import hu.webarticum.miniconnect.util.data.ToStringBuilder;
 
 public final class QueryRequest implements Request, ExchangeMessage {
 
@@ -15,7 +18,7 @@ public final class QueryRequest implements Request, ExchangeMessage {
     public QueryRequest(long sessionId, int exchangeId, String query) {
         this.sessionId = sessionId;
         this.exchangeId = exchangeId;
-        this.query = query;
+        this.query = Objects.requireNonNull(query);
     }
 
 
@@ -31,6 +34,38 @@ public final class QueryRequest implements Request, ExchangeMessage {
 
     public String query() {
         return query;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(exchangeId, query, sessionId);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        } else if (other == null) {
+            return false;
+        } else if (!(other instanceof QueryRequest)) {
+            return false;
+        }
+        
+        QueryRequest otherQueryRequest = (QueryRequest) other;
+        return
+                sessionId == otherQueryRequest.sessionId &&
+                exchangeId == otherQueryRequest.exchangeId &&
+                query.equals(otherQueryRequest.query);
+    }
+
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("sessionId", sessionId)
+                .add("exchangeId", exchangeId)
+                .add("query", query)
+                .build();
     }
 
 }
