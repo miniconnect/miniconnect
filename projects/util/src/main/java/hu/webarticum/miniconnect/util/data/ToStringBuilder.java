07eb32db88106b76d33c6c676532151d7619e89c
Dávid Horváth <horvath@webarticum.hu>
2022-01-22 19:03:25 +0100

    Add ToStringBuilder to util

---
+++ projects/util/src/main/java/hu/webarticum/miniconnect/util/data/ToStringBuilder.java
@@ -0,0 +1,32 @@
+package hu.webarticum.miniconnect.util.data;
+
+public class ToStringBuilder {
+
+    private final StringBuilder stringBuilder = new StringBuilder();
+    
+    private boolean empty = true;
+    
+    
+    public ToStringBuilder(Object object) {
+        stringBuilder.append(object.getClass().getSimpleName());
+        stringBuilder.append(" {");
+    }
+    
+    
+    public ToStringBuilder add(String key, Object value) {
+        if (empty) {
+            empty = false;
+        } else {
+            stringBuilder.append(", ");
+        }
+        stringBuilder.append(key);
+        stringBuilder.append(": ");
+        stringBuilder.append(value);
+        return this;
+    }
+    
+    public String build() {
+        return stringBuilder.toString() + "}";
+    }
+    
+}
