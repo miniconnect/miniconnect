9fbf74d82b208f3aa6dd388a8cf51a48439dd5c7
Dávid Horváth <horvath@webarticum.hu>
2022-02-07 08:51:18 +0100

    Improve record project

--- projects/record/src/main/java/hu/webarticum/miniconnect/record/ResultField.java
+++ projects/record/src/main/java/hu/webarticum/miniconnect/record/ResultField.java
@@ -2,15 +2,18 @@ package hu.webarticum.miniconnect.record;
 
 import hu.webarticum.miniconnect.api.MiniValue;
 
-// TODO: value interpreter
+// FIXME: how to handle primitive types (should we)?
 public class ResultField {
     
     private final MiniValue value;
 
-    // TODO
     private final Object valueInterpreter;
     
 
+    public ResultField(MiniValue value) {
+        this(value, new Object());
+    }
+    
     public ResultField(MiniValue value, Object valueInterpreter) {
         this.value = value;
         this.valueInterpreter = valueInterpreter;
@@ -21,7 +24,67 @@ public class ResultField {
         return value;
     }
     
-    // TODO
-    // TODO: as(...) asInt() etc.
+    public <T> T as(Class<T> clazz) {
+        
+        // TODO
+        return null;
+        
+    }
+    
+    public boolean asBoolean() {
+        
+        // TODO: eliminate autoboxing
+        return as(Boolean.class);
+        
+    }
+    
+    public byte asByte() {
+
+        // TODO: eliminate autoboxing
+        return as(Byte.class);
+        
+    }
+    
+    public char asChar() {
+
+        // TODO: eliminate autoboxing
+        return as(Character.class);
+        
+    }
+    
+    public short asShort() {
+
+        // TODO: eliminate autoboxing
+        return as(Short.class);
+        
+    }
+    
+    public int asInt() {
+
+        // TODO: eliminate autoboxing
+        return as(Integer.class);
+        
+    }
+    
+    public long asLong() {
+
+        // TODO: eliminate autoboxing
+        return as(Long.class);
+        
+    }
+    
+    public float asFloat() {
+
+        // TODO: eliminate autoboxing
+        return as(Float.class);
+        
+    }
+    
+    public double asDouble() {
+
+        // TODO: eliminate autoboxing
+        return as(Double.class);
+        
+    }
     
 }
