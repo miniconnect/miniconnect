48dd56b27575745d5d5059827eaed47b2b8ccc0b
Dávid Horváth <horvath@webarticum.hu>
2022-03-02 19:21:07 +0100

    Improve jdbc type handling in MiniJdbcResultSetMetaData

--- projects/jdbc/src/main/java/hu/webarticum/miniconnect/jdbc/MiniJdbcResultSetMetaData.java
+++ projects/jdbc/src/main/java/hu/webarticum/miniconnect/jdbc/MiniJdbcResultSetMetaData.java
@@ -1,15 +1,60 @@
 package hu.webarticum.miniconnect.jdbc;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Date;
+import java.sql.JDBCType;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.sql.Types;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import hu.webarticum.miniconnect.api.MiniColumnHeader;
+import hu.webarticum.miniconnect.lang.ByteString;
 import hu.webarticum.miniconnect.lang.ImmutableList;
+import hu.webarticum.miniconnect.record.lob.BlobValue;
+import hu.webarticum.miniconnect.record.lob.ClobValue;
 import hu.webarticum.miniconnect.record.translator.ValueTranslator;
 
 public class MiniJdbcResultSetMetaData implements ResultSetMetaData {
 
+    private static final Map<String, JDBCType> TYPE_MAPPING =
+            Collections.synchronizedMap(new HashMap<>());
+    static {
+        // FIXME
+        TYPE_MAPPING.put(Void.class.getName(), JDBCType.NULL);
+        TYPE_MAPPING.put(Boolean.class.getName(), JDBCType.BOOLEAN);
+        TYPE_MAPPING.put(Byte.class.getName(), JDBCType.TINYINT);
+        TYPE_MAPPING.put(Short.class.getName(), JDBCType.SMALLINT);
+        TYPE_MAPPING.put(Integer.class.getName(), JDBCType.INTEGER);
+        TYPE_MAPPING.put(Long.class.getName(), JDBCType.BIGINT);
+        TYPE_MAPPING.put(Float.class.getName(), JDBCType.REAL);
+        TYPE_MAPPING.put(Double.class.getName(), JDBCType.DOUBLE);
+        TYPE_MAPPING.put(BigInteger.class.getName(), JDBCType.NUMERIC);
+        TYPE_MAPPING.put(BigDecimal.class.getName(), JDBCType.DECIMAL);
+        TYPE_MAPPING.put(ByteString.class.getName(), JDBCType.LONGVARBINARY);
+        TYPE_MAPPING.put(byte[].class.getName(), JDBCType.LONGVARBINARY);
+        TYPE_MAPPING.put(Character.class.getName(), JDBCType.CHAR);
+        TYPE_MAPPING.put(String.class.getName(), JDBCType.LONGVARCHAR);
+        TYPE_MAPPING.put(LocalTime.class.getName(), JDBCType.TIME);
+        TYPE_MAPPING.put(Time.class.getName(), JDBCType.TIME);
+        TYPE_MAPPING.put(LocalDate.class.getName(), JDBCType.DATE);
+        TYPE_MAPPING.put(Date.class.getName(), JDBCType.DATE);
+        TYPE_MAPPING.put(java.util.Date.class.getName(), JDBCType.DATE);
+        TYPE_MAPPING.put(Instant.class.getName(), JDBCType.TIMESTAMP);
+        TYPE_MAPPING.put(Timestamp.class.getName(), JDBCType.TIMESTAMP);
+        TYPE_MAPPING.put(BlobValue.class.getName(), JDBCType.BLOB);
+        TYPE_MAPPING.put(ClobValue.class.getName(), JDBCType.CLOB);
+    }
+    
+    
     private final MiniJdbcResultSet resultSet;
 
     private final ImmutableList<String> columnClassNames;
@@ -49,18 +94,22 @@ public class MiniJdbcResultSetMetaData implements ResultSetMetaData {
 
     @Override
     public int getColumnType(int column) throws SQLException {
-        //JDBCType.BIGINT.
-        // FIXME / TODO
-        return Types.JAVA_OBJECT;
+        String columnClassName = getColumnClassName(column);
+        if (!TYPE_MAPPING.containsKey(columnClassName)) {
+            return Types.JAVA_OBJECT;
+        }
         
+        return TYPE_MAPPING.get(columnClassName).getVendorTypeNumber();
     }
 
     @Override
     public String getColumnTypeName(int column) throws SQLException {
+        String columnClassName = getColumnClassName(column);
+        if (!TYPE_MAPPING.containsKey(columnClassName)) {
+            return "JAVA_OBJECT";
+        }
         
-        // FIXME / TODO
-        return "JAVA_OBJECT";
-        
+        return TYPE_MAPPING.get(columnClassName).getName();
     }
 
     @Override
