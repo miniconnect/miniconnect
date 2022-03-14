dd40a26381ecd99880630882b75744c62844eced
Dávid Horváth <horvath@webarticum.hu>
2022-03-14 21:44:51 +0100

    Improve rest project

--- projects/rest/src/main/java/hu/webarticum/miniconnect/rest/Configuration.java
+++ projects/rest/src/main/java/hu/webarticum/miniconnect/rest/Configuration.java
@@ -1,14 +1,29 @@
 package hu.webarticum.miniconnect.rest;
 
+import hu.webarticum.miniconnect.api.MiniSession;
+import hu.webarticum.miniconnect.api.MiniSessionManager;
+import hu.webarticum.miniconnect.jdbcadapter.JdbcAdapterSessionManager;
 import io.micronaut.context.annotation.Factory;
 import jakarta.inject.Singleton;
 
 @Factory
 public class Configuration {
 
+    // TODO: use url from configuration
     @Singleton
-    public TestBean testBean() {
-        return new TestBean("I am the default test bean...");
+    public MiniSession createMiniSession() {
+        String inMemoryConnectionUrl = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=FALSE";
+        MiniSessionManager sessionManager = new JdbcAdapterSessionManager(inMemoryConnectionUrl);
+        MiniSession session = sessionManager.openSession();
+        session.execute("CREATE TABLE data (" +
+                "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "label VARCHAR(255) NOT NULL, " +
+                "description TEXT)");
+        session.execute("INSERT INTO data VALUES (NULL, 'lorem', 'Lorem Ipsum')");
+        session.execute("INSERT INTO data VALUES (NULL, 'hello', 'Hello World')");
+        session.execute("INSERT INTO data VALUES (NULL, 'xxx', 'XXX YYY ZZZ')");
+        session.execute("INSERT INTO data VALUES (NULL, 'abc', 'abc 123 ABC I  II III')");
+        return session;
     }
     
 }
---
+++ projects/rest/src/main/java/hu/webarticum/miniconnect/rest/TableController.java
@@ -0,0 +1,78 @@
+package hu.webarticum.miniconnect.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import hu.webarticum.miniconnect.api.MiniColumnHeader;
+import hu.webarticum.miniconnect.api.MiniResultSet;
+import hu.webarticum.miniconnect.api.MiniSession;
+import hu.webarticum.miniconnect.lang.ImmutableList;
+import hu.webarticum.miniconnect.lang.ImmutableMap;
+import hu.webarticum.miniconnect.record.ResultRecord;
+import hu.webarticum.miniconnect.record.ResultTable;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.exceptions.HttpStatusException;
+
+// TODO: make queries configurable
+@Controller("/tables")
+public class TableController {
+    
+    private final MiniSession session;
+    
+    
+    public TableController(MiniSession session) {
+        this.session = session;
+    }
+    
+    // TODO: make it unified
+    // FIXME: describe?
+    @Get("/")
+    public List<Map<String, Object>> handle() {
+        List<Map<String, Object>> result = new ArrayList<>();
+        MiniResultSet resultSet = session.execute("SHOW TABLES").resultSet();
+        ImmutableList<String> columnNames = resultSet.columnHeaders().map(MiniColumnHeader::name);
+        ResultTable resultTable = new ResultTable(resultSet);
+        for (ResultRecord resultRecord : resultTable) {
+            ImmutableMap<String, Object> row = columnNames.assign(n -> resultRecord.get(n).get());
+            result.add(row.asMap());
+        }
+        return result;
+    }
+
+    // FIXME
+    @Get("/{tableName}")
+    public Map<String, Object> handleTable(
+            @QueryValue("tableName") String tableName) {
+        MiniResultSet resultSet = session.execute("SHOW TABLES").resultSet();
+        ImmutableList<String> columnNames = resultSet.columnHeaders().map(MiniColumnHeader::name);
+        ResultTable resultTable = new ResultTable(resultSet);
+        for (ResultRecord resultRecord : resultTable) {
+            String rowTableName = resultRecord.get(0).as(String.class);
+            if (rowTableName.equals(tableName)) {
+                return columnNames.assign(n -> resultRecord.get(n).get()).asMap();
+            }
+        }
+        throw new HttpStatusException(HttpStatus.NOT_FOUND, "Table not found: " + tableName);
+    }
+
+    @Get("/{tableName}/rows")
+    public List<Map<String, Object>> handleTableData(
+            @QueryValue("tableName") String tableName) {
+        List<Map<String, Object>> result = new ArrayList<>();
+        String quotedTableName = "`" + tableName.replace("`", "``") + "`";
+        String sql = "SELECT * FROM " + quotedTableName;
+        MiniResultSet resultSet = session.execute(sql).resultSet();
+        ImmutableList<String> columnNames = resultSet.columnHeaders().map(MiniColumnHeader::name);
+        ResultTable resultTable = new ResultTable(resultSet);
+        for (ResultRecord resultRecord : resultTable) {
+            ImmutableMap<String, Object> row = columnNames.assign(n -> resultRecord.get(n).get());
+            result.add(row.asMap());
+        }
+        return result;
+    }
+    
+}
