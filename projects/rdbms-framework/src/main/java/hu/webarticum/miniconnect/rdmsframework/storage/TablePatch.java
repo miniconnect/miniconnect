c50163627e2e32ee55f6f6649f7c958709f6cedd
Dávid Horváth <horvath@webarticum.hu>
2022-04-08 00:33:11 +0200

    Implement TablePatch

--- projects/rdbms-framework/src/main/java/hu/webarticum/miniconnect/rdmsframework/storage/TablePatch.java
+++ projects/rdbms-framework/src/main/java/hu/webarticum/miniconnect/rdmsframework/storage/TablePatch.java
@@ -1,7 +1,39 @@
 package hu.webarticum.miniconnect.rdmsframework.storage;
 
+import java.math.BigInteger;
+
+import hu.webarticum.miniconnect.lang.ImmutableList;
+import hu.webarticum.miniconnect.lang.ImmutableMap;
+
 public class TablePatch {
 
-    // TODO: patch structure...
+    private final ImmutableList<Row> insertedRows;
+    
+    private final ImmutableMap<BigInteger, ImmutableMap<Integer, Object>> updates;
+    
+    private final ImmutableList<BigInteger> deletions;
+
+    
+    public TablePatch(
+            ImmutableList<Row> insertedRows,
+            ImmutableMap<BigInteger, ImmutableMap<Integer, Object>> updates,
+            ImmutableList<BigInteger> deletions) {
+        this.insertedRows = insertedRows;
+        this.updates = updates;
+        this.deletions = deletions;
+    }
+
+
+    public ImmutableList<Row> insertedRows() {
+        return insertedRows;
+    }
+
+    public ImmutableMap<BigInteger, ImmutableMap<Integer, Object>> updates() {
+        return updates;
+    }
+
+    public ImmutableList<BigInteger> deletions() {
+        return deletions;
+    }
     
 }
