e983ebe64a48245194e30a80e5db7d8fa0aa27eb
Dávid Horváth <horvath@webarticum.hu>
2022-04-11 22:19:12 +0200

    Add createPredicate() to IndexUtil

--- projects/rdbms-framework/src/main/java/hu/webarticum/miniconnect/rdmsframework/util/IndexUtil.java
+++ projects/rdbms-framework/src/main/java/hu/webarticum/miniconnect/rdmsframework/util/IndexUtil.java
@@ -1,12 +1,14 @@
 package hu.webarticum.miniconnect.rdmsframework.util;
 
 import java.util.Comparator;
+import java.util.function.Predicate;
 
 import hu.webarticum.miniconnect.lang.ImmutableList;
 import hu.webarticum.miniconnect.rdmsframework.storage.Column;
 import hu.webarticum.miniconnect.rdmsframework.storage.ColumnDefinition;
 import hu.webarticum.miniconnect.rdmsframework.storage.NamedResourceStore;
 import hu.webarticum.miniconnect.rdmsframework.storage.Table;
+import hu.webarticum.miniconnect.rdmsframework.storage.TableIndex.InclusionMode;
 import hu.webarticum.miniconnect.rdmsframework.storage.TableIndex.SortMode;
 import hu.webarticum.miniconnect.rdmsframework.storage.impl.simple.MultiComparator;
 import hu.webarticum.miniconnect.rdmsframework.storage.impl.simple.MultiComparator.MultiComparatorBuilder;
@@ -35,6 +37,41 @@ public final class IndexUtil {
             builder.add(columnComparator, nullable, sortMode.isAsc(), sortMode.isNullsFirst());
         }
         return builder.build();
+    }
+    
+    public static Predicate<ImmutableList<Object>> createPredicate(
+            ImmutableList<?> from,
+            InclusionMode fromInclusionMode,
+            ImmutableList<?> to,
+            InclusionMode toInclusionMode,
+            MultiComparator multiComparator) {
+        Predicate<ImmutableList<Object>> result = null;
+        if (from != null) {
+            @SuppressWarnings("unchecked")
+            ImmutableList<Object> fromAsObjects = (ImmutableList<Object>) from;
+            Predicate<ImmutableList<Object>> fromPredicate;
+            if (fromInclusionMode == InclusionMode.INCLUDE) {
+                fromPredicate = v -> multiComparator.compare(v, fromAsObjects) >= 0;
+            } else {
+                fromPredicate = v -> multiComparator.compare(v, fromAsObjects) > 0;
+            }
+            result = fromPredicate;
+        }
+        if (to != null) {
+            @SuppressWarnings("unchecked")
+            ImmutableList<Object> toAsObjects = (ImmutableList<Object>) to;
+            Predicate<ImmutableList<Object>> toPredicate;
+            if (toInclusionMode == InclusionMode.INCLUDE) {
+                toPredicate = v -> multiComparator.compare(v, toAsObjects) <= 0;
+            } else {
+                toPredicate = v -> multiComparator.compare(v, toAsObjects) < 0;
+            }
+            result = result != null ? result.and(toPredicate) : toPredicate;
+        }
+        if (result == null) {
+            result = v -> true;
+        }
+        return result;
     }
 
 }
