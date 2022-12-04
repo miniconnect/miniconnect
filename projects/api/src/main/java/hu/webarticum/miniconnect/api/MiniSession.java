40db8f8053f25a4c4845f74750d46c882204ee98
Dávid Horváth <horvath@webarticum.hu>
2022-12-04 07:50:10 +0100

    Remove a line break

--- projects/api/src/main/java/hu/webarticum/miniconnect/api/MiniSession.java
+++ projects/api/src/main/java/hu/webarticum/miniconnect/api/MiniSession.java
@@ -7,8 +7,7 @@ public interface MiniSession extends Closeable {
 
     public MiniResult execute(String query);
 
-    public MiniLargeDataSaveResult putLargeData(
-            String variableName, long length, InputStream dataSource);
+    public MiniLargeDataSaveResult putLargeData(String variableName, long length, InputStream dataSource);
 
     @Override
     public void close();
