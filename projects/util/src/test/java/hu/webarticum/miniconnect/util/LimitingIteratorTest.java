338a3eaf199c535090b52ea220ff2f1345826483
Dávid Horváth <horvath@webarticum.hu>
2023-02-12 01:58:08 +0100

    Simplify LimitingIteratorTest

--- projects/util/src/test/java/hu/webarticum/miniconnect/util/LimitingIteratorTest.java
+++ projects/util/src/test/java/hu/webarticum/miniconnect/util/LimitingIteratorTest.java
@@ -13,56 +13,56 @@ class LimitingIteratorTest {
     @Test
     void testClearEmpty() {
         Iterator<Integer> baseIterator = Collections.emptyIterator();
-        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 0L);
+        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 0);
         assertThat(iterableOf(limitingIterator)).isEmpty();
     }
 
     @Test
     void testClear() {
         Iterator<Integer> baseIterator = Arrays.asList(3, 2, 3, 4).iterator();
-        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 0L);
+        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 0);
         assertThat(iterableOf(limitingIterator)).isEmpty();
     }
 
     @Test
     void testLimitEmpty() {
         Iterator<Integer> baseIterator = Collections.emptyIterator();
-        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 17L);
+        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 17);
         assertThat(iterableOf(limitingIterator)).isEmpty();
     }
 
     @Test
     void testLimitEmptyToNegative() {
         Iterator<Integer> baseIterator = Collections.emptyIterator();
-        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, -8L);
+        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, -8);
         assertThat(iterableOf(limitingIterator)).isEmpty();
     }
 
     @Test
     void testLimit() {
         Iterator<Integer> baseIterator = Arrays.asList(3, 2, 3, 4, 8, 3, 12, -1).iterator();
-        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 4L);
+        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 4);
         assertThat(iterableOf(limitingIterator)).containsExactly(3, 2, 3, 4);
     }
 
     @Test
     void testLimitToSameSize() {
         Iterator<Integer> baseIterator = Arrays.asList(3, 2, 3, 4, 8, 3, 12, -1).iterator();
-        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 8L);
+        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 8);
         assertThat(iterableOf(limitingIterator)).containsExactly(3, 2, 3, 4, 8, 3, 12, -1);
     }
 
     @Test
     void testLimitToLargerSize() {
         Iterator<Integer> baseIterator = Arrays.asList(3, 2, 3, 4, 8, 3, 12, -1).iterator();
-        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 137L);
+        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, 137);
         assertThat(iterableOf(limitingIterator)).containsExactly(3, 2, 3, 4, 8, 3, 12, -1);
     }
 
     @Test
     void testLimitToNegative() {
         Iterator<Integer> baseIterator = Arrays.asList(3, 2, 3, 4, 8, 3, 12, -1).iterator();
-        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, -5L);
+        LimitingIterator<Integer> limitingIterator = new LimitingIterator<>(baseIterator, -5);
         assertThat(iterableOf(limitingIterator)).isEmpty();
     }
     
